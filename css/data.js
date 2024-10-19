e27677d5b8b5e31e9c757acd403e513db9ea7153
Mr.SASUKE <62269459+rashwan957@users.noreply.github.com>
2024-10-19 14:14:58 +0300

    Create data.js

---
+++ css/data.js
@@ -0,0 +1,679 @@
+
+	 var obj={"a little":	["ንእሽቶይ"	,"ቁሩብ"	,"ውሕድ"]	,
+"a long time":	"ነዊሕ ግዝ"			,
+"hi":	"سڵاو"			,
+"abandon":	["ገደፈ"	,"ረጥረጠ"]		,
+"abandonment":	"ርጥረጣ"			,
+"abbot":	"ኣባ"			,
+"ability":	["ጥበብ"	,"ክእለት"	,"ብቕዓት"]	,
+"able":	["ጥበባዊ","ክእለታዊ"]		,
+"above":	"ኣብ ልዕሊ"			,
+"abrupt":	"ሃንደበት"			,
+"abruptly":"ብጋህዲ"			,
+"abscess":	"ሓገል"	,
+"absence":	"ብኩራት"		,
+"absent":	"ብኹር"			,
+"absent ":	"ቦኾረ"			,
+"absent minded":	"ዝንጉዕ"			,
+"absent mindedness":	"ምዝንጋዕ"			,
+"absolute":	"ፍጹም"			,
+"absolutely":	"ብፍጹም"			,
+"absolve":	"ፈተሐ"			,
+"accustomed":	"ኣልመደ"			,
+"ache":	"ቓንዛ"			,
+"achieve":	"ፈጸመ"			,
+"achievement":	"ጭብጢ"			,
+"aching":	"መቐንዘዊ"			,
+"achy":	"ቓንዛዊ"			,
+"acidic":	"መጺጽ"			,
+"acorn":	"ፍረ ኦክ"			,
+"acquaint":	["ምልላይ"	,"ምፍላጥ"]		,
+"acquaintance":	["ሌላ"	,"ፍልጠት"]		,
+"acquire":	"ረኸበ"			,
+"acquit":	["ፈተሐ"	,"ንጹህ"	,"ቕንዕና"]	,
+"acquittal":	"ምኽፋል ዕዳ"			,
+"act":	["ገበረ"	,"ምግባር"	,"ምትውሳእ"]	,
+"action":	["ተግባር"	,"ግብሪ"]		, 
+"actively":	["ተግባራዊ"	,"ግብራዊ"]		,
+"activity":	["ንጥፈት"	,"ስራሕ"]		,
+"actor":	"ተዋሳአይ"			,
+"actors":	"ተዋሰእቲ"			,
+"actress":	"ተዋሳኢት"			,
+"advantage":	"ረብሓ"			,
+"adventure":	"ዕንደራ"			,
+"adversary":	"ተጻራሪ"			,
+"advertisement":	"ረክላም"			,
+"advice":	"ምኽሪ"			,
+"advise":	"መኸረ"			,
+"adviser":	"ኣማኻሪ"			,
+"advocate":	["ጠበቓ"	,"ወከል"	,"ሐገዝ"]	,
+"advocate":	["ምውካል"	,"ምሕጋዝ"]		,
+"advocator":	["ወካሊት"	,"ሐጋዚት"]		,
+"advocator":	["ወካሊ"	,"ሓጋዚ"]		,
+"advocator":	["ወከልቲ"	,"ሓገዝቲ"]		,
+"aerate":	"ኣናፈሰ"			,
+"affect":	["ሳዕቤን"	,"ተንከፈ"]		,
+"affection":	["ስምዒት"	,"ፍቕሪ"]		,
+"affectionate":	["ስምዒታዊ"	,"ፈቃር"]		,
+"affirm":	["ምርግጋጽ"	,"ኣመስለ"]		,
+"affirmable":	"ርግጻዊ"			,
+"affirmably":	"ብርግጽ"			,
+"afford":	["ምኽኣል"	,"ኪኸፍል"]		,
+"affordable":	"ክኣል"			,
+"air":	["ንፋስ"	,"ኣየር"]		,
+"airplane":	"ነፋሪት"			,
+"airport":	"መዕረፍ ነፈርቲ"			,
+"alarm":	"መጠንቀቕታ"			,
+"alarm":	"ምጥንቓቕ"			,
+"alight ":	"ተባርዐ"			,
+"align":	["ረበበ"	,"ምግጣም"	,"ምስራዕ"]	,
+"alive":	"ሂወታዊ"			,
+"alive":	["ነበረ"	,"ምህላው"]		,
+"all":	"ኩሉ"			,
+"alliance":	["ኪዳን"	,"ምሕዝና"	,"ምሕዝነት"]	,
+"allow":	"ኣፍቀደ"			,
+"alone":	["እንኮ ንብሕቶ"	,"ጽሙዋ"]		,
+"alphabet":	"ፈደል"			,
+"already":	"ድሮ"			,
+"always":	"ኩሉ ግዜ"			,
+"amateur":	"ተልመደን"			,
+"amaze":	["ኣገረመ"	,"ምግራም"	,"ምድናቕ"]	,
+"amazed":	["ኣገራሚ"	,"ኣደናቒ"]		,
+"amazedly":	"ብምግራም"			,
+"amazing":	"ሃንደበታዊ"			,
+"ambition":	"ትምኒት"			,
+"ambulance":	"ኣምቡላንስ"			,
+"amenable":	"ምእዙዝ"			,
+"amends":	"ጽገና"			,
+"amount":	["ድምር"	,"ክንደይ"]		,
+"ample":	"ገፊሕ"			,
+"amuse":	["ኣስሓቐ"	,"ኣዘናገዐ"]		,
+"amusement":	"ምዝንጋዕ"			,
+"amusing":	"መስሓቕ"			,
+"ancestor":	"ወለዶ"			,
+"ancient":	"ጥንታዊ"			,
+"angel":	"መልኣኽ"			,
+"anger":	["ሕርቓን"	,"ኩራ"]		,
+"anger":	["ምሕራቕ"	,"ምኹራይ"]		,
+"angle":	"ኩርናዕ"			,
+"angry":	["ሓራቕ"	,"ኾራዪ"]		,
+"angry":	["ኾራይት"	,"ኩርፍፍቲ"]		,
+"angry":	"ኾርፋፍ"			,
+"anguish":	"ጭንቀት"			,
+"animal":	"እንስሳ"			,
+"animate":	"ህይወት ሃበ"			,
+"ankle":	["ግንቦ እግሪ"	,"ኩርናዕ እግሪ"]		,
+"announce":	"ኣወጀ"			,
+"announcement":	["መግለጺ"	,"ታእዋጅ"	,"መልእኽቲ"]	,
+"annoy":	"ኣሕረቐ"			,
+"annoyed":	"ኮረየ"			,
+"annual":	"ዓመታዊ"			,
+"answer":	"መልሲ"			,
+"answer":	"መለሰ"			,
+"ant":	"ጻጸ"			,
+"ant hill":	"ጉላ"			,
+"antenna":	"ቅርኒብ ሓሸራ"			,
+"anticipated":	"ኣቐዲምካ ዝግበር"			,
+"antique":	"ጥንታዊ"			,
+"antiquity":	"ጥንቲ"			,
+"anvil":	"ወጸቓ"			,
+"anxiety":	["ሃንቀውታ"	,"ጭንቀት"	,"ሽግርግር"]	,
+"anxiety":	"ትሻቐለ"			,
+"anxious":	"ሽጉር"			,
+"any":	"ኩሉ"			,
+"anybody":	"ሰብ"			,
+"anything":	"ገለኳ"			,
+"apartment":	"ገዛ"			,
+"apologize":	"ይቕረታ ሓተተ"			,
+"appalling":	["ኣሰንባዲ"	,"ኣገራሚ"	,"ኣደናቒ"]	,
+"apparatus":	"መሳርሒ"			, 
+"appeal":	["ይግባይ"	,"ሕቶ"]		,
+"appeal":	"ምሕታት"			,
+"appear":	"ተራእየ"			,
+"appear suddenly":	"ተቐልቀለ"			,
+"appearance":	"ምቕልቃል"			,
+"appease":	"ኣህደአ"			,
+"applaud":	["ኣንጨብጨበ"	,"ምጥቓዕ"	]	,
+"applauder":	"ኣጣቓዒት"			,
+"applauder":	"ኣጣቓዓይ"			, 
+"applauder":	"መጣቐዕቲ"			,
+"applause":	["ጨብጨብታ"	,"ጣቕዒት"]		,
+"apple":	["ቱፋሕ"	,"ቱፋህ"	,"ሜለ"]	,
+"apple tree":	"ቱፋሕ ገረብ"			,
+"application":	"ተገዳስነት"			,
+"applied":	"ትጉህ"			,
+"apply":	"ሓተተ"			,
+"apply for":	"ጠለበ"			,
+"appoint":	"ሰየመ"			,
+"appointment":	"ቆጸራ"			,
+"appraise":	"ገመተ"			,
+"appreciable":	["ተነካኢ"	,"ፉሉጥ"	,"ሞገስ"]	,
+"appreciate":	["ኣሞገሰ"	,"ምምስጋን"]		,
+"apprenticeship":	"ልምምድ"			,
+"approach":	"ቀረበ"			,
+"approval":	"ምጽዳቕ"			,
+"approve of":	"ኣጽደቐ"			,
+"apricot tree":	"ሚሽሚሽ ገረብ"			,
+"April":	"ሚያዝያ"			,
+"apron":	"ግርምብያለ"			,
+"aptitude":	"ቅሊ"			,
+"arc":	["ቀሸም"	,"ቀስቲ"]		,
+"ardent":	"ረሳን"			,
+"area":	["ዞባ"	,"ቦታ"]		,
+"argue":	["ተኻትዐ"	,"ምክታዕ"	,"ምቁያቕ"]	,
+"arithmetic":	"ቁጽሪ"			,
+"arm":	"ቅልጽም"			,
+"arm":	"ኣዕጠቐ"			,
+"armchair":	"ኩርሲ"			,
+"armistice":	"ግዚያዊ"			,
+"arms":	"የእዳዎ"			,
+"army":	"ሰራዊት"			,
+"arouse":	"ተበራበረ"			,
+"arrange":	["ኣዳለወ"	,"ምምዕራይ"]		,
+"arrest":	"ማእሰርቲ"			,
+"arrest":	"ኣሰረ"			,
+"arrive":	"በጽሐ"			,
+"art":	["ጥበብ"	,"ስእሊ"]		,
+"artery":	["ኣርተሪ"	,"ሰራውር"]		,
+"article":	"ዓንቀጽ"			,
+"artificial lake":	["ጋብላ"	,"ዒላ"]		,
+"artist":	["ጥበበኛ"	,"ሳኣላይ"]		,
+"as far as":	"ክሳብ"			,
+"as many":	"ብጣዕሚ"			,
+"as much":	"ብጣዕሚ"			,
+"as well":	"ከምኡውን"			,
+"ascent":	"ዕርገት"			,
+"ashamed":	"ዘሕፍር"			,
+"ashes":	"ሓሙኹሽቲ"			,
+"ask":	"ሓተተ"			,
+"assassin":	"ቀታሊ"			,
+"assassinate":	"ቀተለ"			,
+"assemble":	["ኣላገበ"	,"ምግጣም"]		,
+"assembly":	["ባይቶ"	,"ጉባኤ"]		,
+"assert":	"ኣረጋገጸ"			,
+"assess":	"ገመተ"			,
+"assign":	"መዘዘ"			,
+"assist":	"ሓገዘ"			,
+"assistance":	"ሓገዝ"			,
+"association":	"ማሕበር"			,
+"assurance":	"መተኣማመኒ"			,
+"astonish":	["ኣገረሀ"	,"ኣገረመ"]		,
+"astonished ":	"ተገረመ"			,
+"astonishment":	"ድንጽዉና"			,
+"at":	"ኣብ"			,
+"at last":	"ኣብ መዳእታ"			,
+"at least":	["ብብዚሒ"	,"ብዉሕድ"]		,
+"atmosphere":	"ሃዋህው"			,
+"attach oneself to":	"ጠበቐ"			,
+"attachment":	"ጥብቀት"			,
+"attack":	"ኣጥቅዐ"			,
+"attempt":	"ፈተነ"			, 
+"attend":	"ተኻፈለ"			,
+"attention":	["ጥንቃቐ"	,"ኣድህብ"]		,
+"attentive":	["ጥንቁቕ"	,"ኣድሃቢ"]		,
+"attentively":	"ብጥንቃቐ"			,
+"attorney":	"ጠበቓ"			,
+"attract":	"ማረኸ"			,
+"attractiveness":	"ማርኾት"			,
+"attribute":	"መዘዘ"			,
+"August":	"ነሓሰ"			,
+"auntaunt":	["ኣሞሓትነ"	,"ሓትኖ"]		,
+"author":	["ደራሲ"	,"ደራሳይ"	,"ጸሓፋይ"]	,
+"authority":	"ሰበ ስልጣን"			,
+"authorize":	"ኣፍቀደ"			,
+"automatic":	"ኣውቶማቲክ"			,
+"autumn":	"ቀውዒ"			,
+"autumnal":	"ናይ ቀውዒ"			,
+"avenge":	"ሕነ ፈደየ"			,
+"avenue":	"ዓብይ ጎደና"			,
+"avert":	"ፈላለየ"			,
+"avoid":	"ወገደ"			,
+"awakening":	"ሰዓት ደወል"			,
+"award":	"ብልጫ"			,
+"aware of":	"ፈለጠ"			,
+"away":	"ወሰደ"			,
+"away":	"ኣለየ"			,
+"axe":	"ፋስ"	,
+          "babble":	"ኣዕዘምዘመ"			,
+"baby":	"ሕጻን"			,
+"bachelor":	"ዘይምርዕዉ"			,
+"back ":	"ሕቆ"			,
+"back ":	"ድሕሪት"			,
+"back away":	"ደሓረ"			,
+"back":	["go"	,"ተመለሰ"]		,
+"back":	["take"	,"መለሰ"]		,
+"background":	"ማዓሙቕ"			,
+"backward":	"ድሕሪት"			,
+"bad":	"ሕማቕ"			,
+"bad luck":	"ሕማቕ ዕድል"			,
+"bag":	"ሳንጣ"			,
+"baggage":	["ባልጃ"	,"ኣቑሑ ጉዕዞ"]		,
+"bait":	"መቑለብ ዓሳ"			,
+"baker":	"ሰንካቲ"			,
+"baker's shop":	"እንዳ ባኒ"			,
+"balance":	"ሚዛን"			,
+"balcony":	"ባልኮን"			,
+"ball":	"ኩዕሶ"			,
+"ban":	"ወገደ"			,
+"banana":	"ባናና"			,
+"band":	"መጥመሪ"			,
+"bandage":	["ኣተሓኻኸማ"	,"ቺረቶ"]		,
+"bandit":	"ሽፍታ"			,
+"bang":	["ዘበጠ"	,"ምህራም"	,"ምዝባጥ"]	,
+"bang on":	"ወቕዐ"			,
+"bank":	"ባንክ"			,
+"banker":	"ባንከኛ"			,
+"baptism":	"ጥምቀት"			,
+"bar":	["መደንደል"	,"ቤት መስተ"]		,
+"barbed wire":	["ትሪኮላታ"	,"እሾኽ"]		,
+"barge":	"ባርካ"			,
+"bark":	["ቅላጥ"	,"ቅላጥ ዕንጨይቲ"]		,
+"bark":	"ነብሐ"			,
+"barley":	"ስገም"			,
+"barn":	"መኽዘን አኽሊ"			,
+"barometer":	"ባሮሜተር"			,
+"barrel":	["መድፍዕ"	,"ፊስቶ"]		,
+"barren":	"መኻን"			,
+"barrier":	"መጋረዲ"			,
+"barring":	"ግድብ"			,
+"barrister":	"ጠበቓ"			,
+"basil":	["ቤዚል"	,"ባዘሊኮ"]		,
+"basin":	"ጋብላ"			,
+"basket":	"ዘንቢል"			,
+"bath":	"ምሕጻብ ነብሲ"			,
+"bathe":	"ተሓጸበ"			,
+"baton":	"ዘንጊ"			,
+"battalion":	"ቦጦሎኒ"			,
+"battle":	"ውግእ"			,
+"battlefield":	"ዓውደ ውግእ"			,
+"bazaar":	["ባዛር"	,"ዕዳጋ"]		,
+"be":	["እዩ","ኣሎ"]		,
+"beach":	"ገምገም ባሕሪ"			,
+"bead":	"ሉል"			,
+"beam":	"ጩራ"			,
+"bean":	"ፋጆሊ"			,
+"beans":	"ባሎንኌ"			,
+"beans stew":	"ትምቲሞ"			,
+"bear":	"ድቢ"			,
+"bear out":	"ኣጸረየ"			,
+"beard":	"ጭሕሚ"			,
+"beast":	"እንስሳ"			,
+"beat":	["ሃረመ"	,"ሰዓረ"]		,
+"beautiful":	"ጽቡቕ"			,
+"beauty":	"ጽባቐ"			,
+"become":	"ኮነ"			,
+"become rich":	"ሃብተመ"			,
+"bed":	["ዓራት","ደቀሰ"]			,
+"bedroom":	"መደቀሲ ከፍሊ"			,
+"bee":	"ንህቢ"			,
+"beech":	"ቢቸ"			,
+"birth":	"ሓረሰት"		, 
+"birthday":	"ልደት"			,
+"biscuit":	"ቢስኮቲ"			,
+"bishop":	"ኣቡን"			,
+"bit":	"ቁራጽ"			,
+"bite":	"ነኸሰ"			,
+"bitter":	"መሪር"			,
+"bizarre":	"ዘገርም"			,
+"black":	"ጸሊም"			,
+"black pepper":	"በርበረ ጸሊም"			,
+"blackbird":	"ዋሪ"			,
+"blackboard":	"ሰሌዳ"			,
+"bladder":	"ፊሕኛ"			,
+"blame":	"ከሰሰ"			,
+"blanket":	"ኮቦርታ"			,
+"blaze":	"ተቓጸለ"			,
+"bleat":	"እምቤዕ"		,
+"bleed":	"ደመየ"			,
+"bless":	"መረቐ"			,
+"blessing":	"ምርቓ"			,
+"blind":	"ዓይነ ስዉር"			,
+"block":	"ወተፈ"			,
+"blond":	"ጨዓይ"			,
+"blood":	"ደም"			,
+"bloody":	"ደማዊ"			,
+"bloom":	"ዓምበበ"			,
+"blossom":	"ዕምባባ"			,
+"blot":	"ንጣብ ርስሓት"			,
+"blotting paper":	"ሰታይ ቀለም"			,
+"blouse":	"ካሚሻ"			,
+"blow":	["ነፈሰ"	,"ኡፍ በለ"]		,
+"blow up":	["ተባርዐ"	,"ነፍሐ"]		,
+"blue":	"ሰማያዊ"			,
+"board":	"ፈራዶ"			,
+"boast":	"ፈከረ"			,
+"boat":	"መርከብ"			,
+"boathouse":	"ገበላ"			,
+"bobbin":	"ቁኒን"			,
+"body":	"ነብሲ"			,
+"boggy":	"ጭቃዊ"			,
+"boiled beef":	"ብሱል ስጋ"			,
+"bomb":	"ቦምባ"			,
+"bombard":	"ደብደበ"			,
+"bombardment":	"ደብዳብ"			,
+"bone":	"ዓጽሚ"			,
+"bones":	"ዓጽሚ"			,
+"book":	"መጽሓፍ"			,
+"booklet":	"መጽሓፍ ጸብጻብ"			,
+"bookstore":	"ድኳን መጻሕፍቲ"			,
+"boot":	"ነዊሕ ሳእኒ"			,
+"border":	"ዶብ"			,
+"boring":	"መሰልከዊ"			,
+"born":	"ተወለደ"			, 
+"borrow":	"ተለቀሐ"			,
+"boss":	"ኣስራሒ"			,
+"bother":	"ኣጨነቐ"			,
+"bottle":	"ጥርሙዝ"			,
+"boulevard":	"ገፊሕ ጎደና"			,
+"boundary":	"ደረት"			,
+"bouquet":	"ጥማር"			,
+"boutique":	"ድኳን"			,
+"box":	"ሳንዱቕ"			,
+"boy":	"ወዲ"			,
+"boyhood":	"ንእስነት"			,
+"brace":	"ኣሰረ"			,
+"bracelet":	"በናጅር"			,
+"brain":	"ሓንጎል"			,
+"branch":	"ቃራና"			,
+"brass band":	"ጥሩምበኛታት"			,
+"brave":	"ተባዕ"			,
+"bravery":	"ትብዓት"			,
+"bread":	"ባኒ"			,
+"bread":	"ቂጫ"			,
+"bread":	"thick unleavened"			,
+"breadth":	"ስፍሓት"			,
+"break up":	"ሰበረ"			,
+"breakdown":	"ስባር"			,
+"breakfast":	"ቁርሲ"			,
+"breaking open":	"ስባር"			,
+"breast":	["ኣፍልቢ"	,"ጡብ"]		,
+"breath":	"ትንፋስ"			,
+"breathe":	"ኣተንፈሰ"			,
+"breed":	"ኣዕበየ"			,
+"brick":	"ሑጡብ"			,
+"bridge":	"ድልድል"			,
+"brief":	"ብሓጺሩ"			,
+"briefcase":	"ቦርሳ"			,
+"bright":	"ብሩህ"			,
+"brightness":	"ብራሃን"			,
+"brilliant":	"ኣንጸባረቒ"			,
+"bring":	"ኣምጸአ"			,
+"bring close":	"ቀረበ"			,
+"bringing together":	"ኣኼባ"			,
+"broad":	"ገፊሕ"			,
+"brochure":	"መጽሓፍ ጸብጻብ"			,
+"broom":	"መኾስተር"			,
+"brother":	"ሓወ"			,
+"brotherly":	"ሕውነታዊ"			,
+"brown":	"ቡናዊ"			,
+"brush":	"ኣስባስላ"			,
+"brutal":	"ኣረመና"			,
+"bucket":	"ሳንኮሎ"			,
+"buddy":	"መሓዛ"			,
+"build":	"ሃነጸ"			,
+"building":	"ህንጻ"			,
+"bulb":	"ኣምፑል"			,
+"bulk":	"እኩብ"			,
+"bull":	"ኣርሓ"			,
+"bullet":	"ጥይት"			,
+"bunch":	"ጥማር"			,
+"bundle":	"ጥቕላል ባኮ"			,
+"burden":	"ጽዕነት"			,
+"burgle":	"ሰረቐ"			,
+"burial":	"ቀብሪ"			,
+"burn":	"ነደደ"			,
+"burning":	"ዘቃጽል"			,
+"burst":	"ነቶገ"			,
+"bush":	"ቆትቋጥ"			,
+"bushy":	"ጽዑቕ"			,
+"business":	"ጉዳይ"			,
+"but":	["ግን"	,"ግና"	,"ጌና"]	,
+"butcher's shop":	"እንዳ ስጋ ሓሰማ"			,
+"butter":	"ጠስሚ"			,
+"butter milk":	"ማይ ጨባ"			,
+"butterfly":	"ጽምብላሊዕ"			,
+"button":	"መልጎም"			,
+"buy":	"ዓደገ"			,
+"buyer":	"ዓዳጊ"			,
+"buzz":	"ዚዝ በለ"			,
+"by all means":	"ብፍጹም"	,
+          "cabbage":	"ካውሎ"			,
+"cage":	"ጋብያ"			,
+"cake":	"ዶልሺ"			,
+"calculate":	"ጸብጸበ"			,
+"calculation":	"ገምጋም"			,
+"calculator":	"መተሓሳሰቢ"			,
+"calendar":	"ዓውደ ኣዋርሕ"			,
+"calf":	["ምራኽ"	,"ዳንጋ"]		,
+"call":	"ጸውዐ"			,
+"calm":	["ሃድአ"	,"ህዱእ"]		,
+"camel":	"ገመል"			,
+"camp":	"መዓስከር"			,
+"candle":	"ሽምዓ"			,
+"candy":	"ካራመለ"			,
+"cane":	"ምርኩስ"			,
+"cap":	"ቆቢዕ"			,
+"capable":	"ኪኢላ"			,
+"capital":	"ርእሲ ከተማ"			,
+"captain":	"ካፒታኖ"			,
+"car":	"መኪና"			,
+"card":	"ካርታ"			,
+"card game":	"ጣላዕ"			,
+"cardboard":	"ካርቶን"			,
+"care":	"ክንከን"			,
+"care for":	"ተኸናኸነ"			,
+"career":	"መስርሕ ሂወት"			,
+"carefully":	"ቀስ"			,
+"carefulness":	"ጥንቃቐ"			,
+"careless":	"ዘይግደስ"			,
+"caretaker":	"ሓላው ርሻን"			,
+"carpenter":	"ናሕሲ ዝሰርሕ"			,
+"carpet":	"ምንጻፍ"			,
+"carriage":	"ባጎኒ"			,
+"carrot":	"ካሮት"			,
+"carry":	"ተሰከመ"			,
+"carry out":	"ኣብ ግብሪ ኣውዓለ"			,
+"cart":	"ካሮሳ"			,
+"carve":	"ቀረጸ"			,
+"case":	"ጉዳይ"			,
+"cashier":	"ተሓዝ ገንዘብ"			,
+"casing":	"ጋብያ"			,
+"castle":	"ኣዳራሽ"			,
+"cat":	"ድሙ"			,
+"catch":	"ቈለበ"			,
+"catch again":	"እንደገና ሓዘ"			,
+"caterpillar":	"ኣብ ጨጎራ"			,
+"cathedral":	"ቤተ ክርስቲያን"			,
+"Catholic":	"ኮተሊኽ"			,
+"Catholicism":	"ኩትልኽና"			,
+"cauliflower":	["ካውሎ ፍዮሪ"	,"ካውሎ ፊየሪ"]		,
+"cause":	["ጠንቂ"	,"ምኽንያት"]		,
+"cave":	"በዓቲ"			,
+"ceiling":	"ትሕቲ ናሕሲ"			,
+"celebrate":	"ኣብዓለ"			,
+"celery":	"ሰዴኖ"			,
+"cement":	"ቸመንቶ"			,
+"cemetery":	"መቓብር"			,
+"cent":	"ሳንቲም"			,
+"center":	"ማእከል"			,
+"centimetre":	"ሰንቲ ሜተረ"			,
+"centre of the eye":	["መርዓት"	,"ዓይኒ"]		,
+"century":	"ክፍለ ዘመን"			,
+"ceremony":	"ጽምብል"			,
+"certain":	"ርግጸኛ"			,
+"certainly":	"ብርግጽ"			,
+"certainty":	"ርግጽነት"			,
+"certificate":	"ባጀላ"			,
+"certify":	"መስከረ"			,
+"chain":	"ሰንሰለት"			,
+"chair":	["ሴዳ"	,"መንበር"]		,
+"chalk":	"ኩርሽ"			,
+"chance":	"ኣጋጣሚ"			,
+"change":	"ቀየረ"			,
+"channel":	"መትረብ"			,
+"chapel":	"ቤተ ጸሎት"			,
+"chapter":	"ምዕራፍ"			,
+"character":	"ፍሉጥ ሰብ"			,
+"charge":	"ጸዓነ"			,
+"charity":	"ምጽዋት"			,
+"charm":	"ምጥውና"			,
+"charming":	"ምጥው"			,
+"chase":	"ሃደነ"			,
+"chasm":	"ጸድፊ"			,
+"chatter":	"ኣዕለለ"			,
+"chauffeur":	"ዘዋር መኪና"			,
+"cheat":	"ኣታለለ"			,
+"check":	"ኣጸረየ"			,
+"cheek":	["ኑንጪ"	,"ምዕጉርቲ"	,"ሙዕጉርቲ"]	,
+"cheer":	"ኣጨብጨበ"			,
+"cheer up":	"ኣተባብዐ"			,
+"cheerfulness":	"ሓጎስ"			,
+"cheese":	"ፎርማጆ"			,
+"chemistry":	"ኬሚስትራ"			,
+"cheque":	"ቸክ"			,
+"cherish":	"ኣሐንቀቐ"			,
+"cherry tree":	"ቸሪ ገረብ"			,
+"chest":	"ኣፍ ልቢ"			,
+"chest of drawers":	"ኮሞ"			,
+"chestnut":	"ፍረ ቸስትናት"			,
+"chestnut tree":	"ገረብ ቸስትናት"			,
+"chick":	"ጫቚት"			,
+"chicken":	["ደርሆ"	,"ጫቚት ደርሆ"]		,
+"chicken stew":	["ኣልጫ"	,"ዝግኒ"]		,
+"youth":	"ንእስነት"			,
+"zeal":	"ተመስጦ"
+		
+}
+
+
+ 
+  
+function switchTranslation(){
+        /*select language from combobox.index 0 indicates the first order from the list which is english to tigrigna
+        you can switch also to the second list order of the combobox content index 1 Tigrigna to enlglish.
+        when selection selected all the page content  will be converted to the selected language.*/
+  var selIdx = document.getElementById("comboboxId1").selectedIndex;
+
+  
+ switch(selIdx){
+        case 0:
+        //the english-Tigrigna language selection
+         document.getElementById("pIdTitle").innerHTML="<b>"+"The language you need to translate from "+"</b>";
+         document.getElementById("pIdInput").innerHTML="Type the word below please";
+         document.getElementById("pIdResult").innerHTML="Here below is the translated word";
+         document.getElementById("btnId1").innerHTML="Translate";
+         document.getElementById("txtInputId").value="";
+         document.getElementById("txtResultId").value="";
+        break;
+        case 1:
+        //the Tigrigna-English language selection
+        document.getElementById("pIdTitle").innerHTML="ክትቅይርዎ ደሊኹም ዘለኹም ቓንቓ ";
+        document.getElementById("pIdInput").innerHTML="ክትርጎሞልኩም ዝደለኹሞ ቃል ኣብዚ የእትዉ።";
+        document.getElementById("pIdResult").innerHTML="ትርጉም ናይ ዘእተኹሞ ቃል እንሆ።";
+        document.getElementById("btnId1").innerHTML="ተርጉም";
+         document.getElementById("txtInputId").value="";
+         document.getElementById("txtResultId").value="";
+        break;
+ }
+ 
+}
+function switchTranslation2(){
+  var selIdLanguageParagraph = document.getElementById("languageInputId").selectedIndex;
+ switch(selIdLanguageParagraph){
+  case 0:
+         document.getElementById("pLanId").innerHTML="Write a sentence here.";
+         document.getElementById("pLanResultId").value="Translated sentence";
+         break;
+   case 1:
+         document.getElementById("pLanId").innerHTML="Schrijf een zin hete.";
+         document.getElementById("pLanResultId").value="Vertaalde zin"
+         break;
+   case 2:
+         document.getElementById("pLanId").innerHTML="ሙሉእ ሓሳባት ኣብዚ ጸሓፉ ?";
+         document.getElementById("pLanResultId").value="ትርጉም ናይ ሙሉእ ሓሳብት እንሆ።";
+         break;
+ }
+
+}
+function switchTranslation3(){
+  var selIdLanguageParagraphOutput = document.getElementById("languageOutputId").selectedIndex;
+
+ switch(selIdLanguageParagraphOutput){
+  case 0:
+         document.getElementById("pLanResultId").value="Translated sentence";
+         break;
+   case 1:
+         document.getElementById("pLanResultId").value="Vertaalde zin"
+         break;
+   case 2:
+         
+         document.getElementById("pLanResultId").value="ትርጉም ናይ ሙሉእ ሓሳብት እንሆ።";
+         break;
+ }
+
+}
+
+function comboboxSelectChange(){
+ //user input value must be from the list of the textbox content
+ document.getElementById("txtResultId").value = document.getElementById("list1").value;
+}
+
+function txtOnFocus(){
+        /* when the user focuses on the text input the list of words will be populated on the text input so you can be 
+        able to select from the list and will be automatically alphebatically ordered as you type the word.*/
+        var selIdx = document.getElementById("comboboxId1").selectedIndex;
+        switch(selIdx){
+                case 0:
+                 var output5="";
+                for(var key in obj){
+                output5 +="<option value="+key+">";
+                 }
+                
+                 document.getElementById("list1").innerHTML=output5;
+                 break;
+                 case 1:
+                  var output5="";
+                  //loop through the objects of the language word list
+                  for(var key in obj){
+                  output5 +="<option value="+obj[key]+">";
+                  }
+                  
+                 document.getElementById("list1").innerHTML=output5;
+                 break;
+
+        }
+      
+   }    
+
+  function btnTranslate(){
+ //when button translate clicked the input word will be translated to the selected language
+ //according to the combobox selection which is the index of the selected language an output
+ //will come on the result box .
+ var txtvalue=document.getElementById("txtInputId").value;
+ var selIdx = document.getElementById("comboboxId1").selectedIndex;
+ 
+ switch(selIdx){
+        case 0:
+        //when English-Tigringa language is selected and a word is being written on the text input
+        //the crosponding value of that input is being vissible on the text output.
+         var output=obj[txtvalue];
+         document.getElementById("txtResultId").value=output;
+         break;
+        case 1:
+        //iterate through list of the objects of Tigrigna words at the dictionary.
+        for(var key in obj){
+                //if the crosponding value of the tigrigna word is found on the list then skip out the itration.otherwise continoue
+                //finding it through the whole words.
+                if (!obj.hasOwnProperty(key)) {
+                continue;
+                    }
+               //if the crosponding word is found then display it on the text output.
+               if (txtvalue==obj[key]){
+                   document.getElementById("txtResultId").value=key;
+                   break;
+               }
+        }
+       
+ }
+
+}
